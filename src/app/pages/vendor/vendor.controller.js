6871b2d695b81efefa18aeaab7815fb0e112d21d
Cai, Jade <a600331@fmr.com>
2017-06-21 18:26:31 +0800

    Jade C - add vendor process

--- src/app/pages/vendor/vendor.controller.js
+++ src/app/pages/vendor/vendor.controller.js
@@ -5,7 +5,29 @@
 
     function VendorController($scope, $http, ADMIN_VALUE, $state) {
         var vm = this;
-        $http.post(ADMIN_VALUE.URL_VENDOR_LIST)
+        getVendorList();
+
+        $scope.addVendor = function(){
+            $state.go('vendor.add');
+        }
+        $scope.removeVendor = function(id){
+            console.log(id);
+
+        }
+        $scope.formData = {};
+        $scope.addVendorProcess = function(){
+            $scope.formData.action = "add";
+            $http.post(ADMIN_VALUE.URL_VENDOR_ACTION, $scope.formData)
+            .then(function(res){
+                if (res.data.success === 'true') {
+                    getVendorList();
+                }
+            }, function(res){
+
+            });
+        }
+        function getVendorList(){
+            $http.post(ADMIN_VALUE.URL_VENDOR_LIST)
             .then(function(res){
                 if (res.data.success === 'true') {
                     $scope.vendors = res.data.result.vendor;
@@ -13,13 +35,6 @@
             }, function(res){
 
             });
-
-        $scope.addVendor = function(){
-            $state.go('vendor.add');
-        }
-        $scope.removeVendor = function(id){
-            console.log(id);
-            
         }
     }
 })();
