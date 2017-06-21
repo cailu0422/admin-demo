7de02127075623289294b095aa7b30e8948f0365
cailu <cailu@cailudeMac-mini.lan>
2017-06-22 01:00:05 +0800

    add delete confirm & add success modal

--- src/app/pages/vendor/vendor.controller.js
+++ src/app/pages/vendor/vendor.controller.js
@@ -3,8 +3,9 @@
 
     angular.module('BlurAdmin.pages.vendor').controller('VendorController', VendorController);
 
-    function VendorController($scope, $http, ADMIN_VALUE, $state) {
+    function VendorController($scope, $http, ADMIN_VALUE, $state, $uibModal) {
         var vm = this;
+        $scope.formData = {};
         getVendorList();
 
         $scope.addVendor = function(){
@@ -15,22 +16,16 @@
                 action : "delete",
                 vendor_id : id
             }
-            $http.post(ADMIN_VALUE.URL_VENDOR_ACTION, params)
-            .then(function(res){
-                if (res.data.success === 'true') {
-                    getVendorList();
-                }
-            }, function(res){
-
-            });
+            open('app/pages/vendor/widgets/dangerModal.html', params);
         }
-        $scope.formData = {};
+
         $scope.addVendorProcess = function(){
             $scope.formData.action = "add";
             $http.post(ADMIN_VALUE.URL_VENDOR_ACTION, $scope.formData)
             .then(function(res){
                 if (res.data.success === 'true') {
                     getVendorList();
+                    open('app/pages/vendor/widgets/successModal.html');
                 }
             }, function(res){
 
@@ -46,5 +41,36 @@
 
             });
         }
+        function open(page, params){
+          var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: page,
+            controller: function($scope, $uibModalInstance){
+                $scope.ok = function () {
+                  $uibModalInstance.close();
+                };
+                $scope.delete = function(){
+                    $http.post(ADMIN_VALUE.URL_VENDOR_ACTION, params)
+                    .then(function(res){
+                        if (res.data.success === 'true') {
+                            getVendorList();
+                            $uibModalInstance.close();
+                        }
+                    }, function(res){
+                        $uibModalInstance.close();
+                    });
+                }
+            },
+            resolve: {
+              items: function () {
+                return $scope.items;
+              }
+            }
+          });
+          modalInstance.result.then(function(result){
+            $scope.formData = {};
+          });
+
+        };
     }
 })();
