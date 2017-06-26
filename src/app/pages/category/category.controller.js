b815afbad21cb01ee4be5c0370ae2847de559eb2
Cai, Jade <a600331@fmr.com>
2017-06-26 17:56:27 +0800

    Jade C - add category

---
+++ src/app/pages/category/category.controller.js
@@ -0,0 +1,89 @@
+(function() {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.category').controller('CategoryController', CategoryController);
+
+    function CategoryController($scope, $http, ADMIN_VALUE, $state, $uibModal) {
+        var vm = this;
+        $scope.formData = {};
+        $scope.selectedVendor = {};
+        getCategoryList();
+        getVendorList();
+
+        $scope.addCategory = function(){
+            $state.go('category.add');
+        }
+        $scope.removeCategory = function(id){
+            var params = {
+                action : "delete",
+                category_id : id
+            }
+            open('app/pages/category/widgets/dangerModal.html', params);
+        }
+
+        $scope.addCategoryProcess = function(){
+            $scope.formData.action = "add";
+            console.log($scope.selectedVendor);
+            // $http.post(ADMIN_VALUE.URL_CATEGORY_ACTION, $scope.formData)
+            // .then(function(res){
+            //     if (res.data.success === 'true') {
+            //         getCategoryList();
+            //         open('app/pages/category/widgets/successModal.html');
+            //     }
+            // }, function(res){
+
+            // });
+        }
+        function getCategoryList(){
+            $http.post(ADMIN_VALUE.URL_CATEGORY_LIST)
+            .then(function(res){
+                if (res.data.success === 'true') {
+                    $scope.categories = res.data.result.category;
+                }
+            }, function(res){
+
+            });
+        }
+        function getVendorList(){
+            $http.post(ADMIN_VALUE.URL_VENDOR_LIST)
+            .then(function(res){
+                if (res.data.success === 'true') {
+                    $scope.vendors = res.data.result.vendor;
+                }
+            }, function(res){
+
+            });
+        }
+        function open(page, params){
+          var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: page,
+            controller: function($scope, $uibModalInstance){
+                $scope.ok = function () {
+                  $uibModalInstance.close();
+                };
+                $scope.delete = function(){
+                    $http.post(ADMIN_VALUE.URL_CATEGORY_ACTION, params)
+                    .then(function(res){
+                        if (res.data.success === 'true') {
+                            getCategoryList();
+                            $uibModalInstance.close();
+                        }
+                    }, function(res){
+                        $uibModalInstance.close();
+                    });
+                }
+            },
+            resolve: {
+              items: function () {
+                return $scope.items;
+              }
+            }
+          });
+          modalInstance.result.then(function(result){
+            $scope.formData = {};
+          });
+
+        };
+    }
+})();
---
+++ src/app/pages/category/category.module.js
@@ -0,0 +1,38 @@
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.category', [])
+      .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('category', {
+          url: '/category',
+          template : '<ui-view  autoscroll="true" autoscroll-body-top></ui-view>',
+          abstract: true,
+          controller: 'CategoryController',
+          title: 'Category',
+          sidebarMeta: {
+            icon: 'ion-grid',
+            order: 400,
+          },
+        }).state('category.list', {
+          url: '/list',
+          templateUrl: 'app/pages/category/category.html',
+          title: 'Category Tables',
+          sidebarMeta: {
+            order: 0,
+          },
+        }).state('category.add', {
+          url: '/add',
+          templateUrl: 'app/pages/category/addCategory/addCategory.html',
+          title: 'Add Category',
+          sidebarMeta: {
+            order: 100,
+          },
+        });
+    $urlRouterProvider.when('/category','/category/list');
+  }
+
+})();
