8301b209a4cf9cbe8b6d6fc2b2993fdb47621123
Cai, Jade <a600331@fmr.com>
2017-07-03 17:58:35 +0800

    Jade C - add posts menu

---
+++ src/app/pages/posts/posts.controller.js
@@ -0,0 +1,79 @@
+(function() {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.posts').controller('PostsController', PostsController);
+
+    function PostsController($scope, $http, ADMIN_VALUE, $state, $uibModal) {
+        var vm = this;
+        $scope.formData = {};
+        getVendorList();
+
+        $scope.addVendor = function(){
+            $state.go('vendor.add');
+        }
+        $scope.removeVendor = function(id){
+            var params = {
+                action : "delete",
+                vendor_id : id
+            }
+            open('app/pages/vendor/widgets/dangerModal.html', params);
+        }
+
+        $scope.addVendorProcess = function(){
+            $scope.formData.action = "add";
+            $http.post(ADMIN_VALUE.URL_VENDOR_ACTION, $scope.formData)
+            .then(function(res){
+                if (res.data.success === 'true') {
+                    getVendorList();
+                    open('app/pages/vendor/widgets/successModal.html');
+                }
+            }, function(res){
+
+            });
+        }
+        function getVendorList(){
+            var params = {
+              limit : 99999
+            }
+            $http.post(ADMIN_VALUE.URL_VENDOR_LIST, params)
+            .then(function(res){
+                if (res.data.success === 'true') {
+                    $scope.vendors = res.data.result.vendor;
+                }
+            }, function(res){
+
+            });
+        }
+        function open(page, params){
+          var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: page,
+            controller: function($scope, $uibModalInstance){
+                $scope.ok = function () {
+                  $uibModalInstance.close();
+                };
+                $scope.delete = function(){
+                    $http.post(ADMIN_VALUE.URL_VENDOR_ACTION, params)
+                    .then(function(res){
+                        if (res.data.success === 'true') {
+                            getVendorList();
+                            $uibModalInstance.close();
+                        }
+                    }, function(res){
+                        $uibModalInstance.close();
+                    });
+                }
+            },
+            resolve: {
+              items: function () {
+                return $scope.items;
+              }
+            }
+          });
+          modalInstance.result.then(function(result){
+            $scope.formData = {};
+          });
+
+        };
+    }
+})();
---
+++ src/app/pages/posts/posts.module.js
@@ -0,0 +1,52 @@
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.posts', [])
+      .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('posts', {
+          url: '/posts',
+          template : '<ui-view  autoscroll="true" autoscroll-body-top></ui-view>',
+          abstract: true,
+          controller: 'PostsController',
+          title: 'Posts',
+          sidebarMeta: {
+            icon: 'ion-grid',
+            order: 500,
+          },
+        }).state('posts.pending', {
+          url: '/pending',
+          templateUrl: 'app/pages/posts/pending/pending.html',
+          title: 'Pending Posts',
+          sidebarMeta: {
+            order: 0,
+          },
+        }).state('posts.verifying', {
+          url: '/verifying',
+          templateUrl: 'app/pages/posts/verifying/verifying.html',
+          title: 'Verifying Posts',
+          sidebarMeta: {
+            order: 100,
+          },
+        }).state('posts.approved', {
+          url: '/approved',
+          templateUrl: 'app/pages/posts/approved/approved.html',
+          title: 'Approved Posts',
+          sidebarMeta: {
+            order: 200,
+          },
+        }).state('posts.rejected', {
+          url: '/rejected',
+          templateUrl: 'app/pages/posts/rejected/rejected.html',
+          title: 'Rejected Posts',
+          sidebarMeta: {
+            order: 300,
+          },
+        });
+    $urlRouterProvider.when('/posts','/posts/pending');
+  }
+
+})();
